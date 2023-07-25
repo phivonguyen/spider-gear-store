73f6b2a72dea9a4e1f260e66adba7f01442dc2c6
Jackie <1121@generative.xyz>
2023-07-25 13:58:52 +0700

    make routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/* ================================================== Account ================================================== */
+
+Route::get('/login', function () {
+    return view('account.login.index');
+});
+Route::get('/register', function () {
+    return view('account.register.index');
+});
+Route::profile('/profile', function () {
+    return view('clients.profile.index');
+});
+
 /* ================================================== User ================================================== */
 
 Route::get('/', function () {
@@ -25,6 +37,33 @@ Route::get('/products', function () {
 });
 Route::get('/product-detail', function () {
     return view('clients.product-detail.index');
+});
+Route::get('/cart', function () {
+    return view('clients.cart.index');
+});
+Route::get('/blogs', function () {
+    return view('clients.blogs.index');
+});
+Route::get('/blog-detail', function () {
+    return view('clients.blog-detail.index');
+});
+Route::get('/why', function () {
+    return view('clients.why.index');
+});
+Route::get('/about', function () {
+    return view('clients.about.index');
+});
+Route::get('/cart', function () {
+    return view('clients.cart.index');
+});
+Route::get('/error', function () {
+    return view('clients.error.index');
+});
+Route::get('/check-out', function () {
+    return view('clients.check-out.index');
+});
+Route::get('/check-success', function () {
+    return view('clients.check-success.index');
 });
 
 
